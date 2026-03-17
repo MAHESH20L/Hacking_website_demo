82336f5271d5a7e720b4db8fa890f6bef03bd7eb
PATTIMANI MAHESH <mahibunny.34.s@gmail.com>
2026-03-17 19:40:08 +0530

    Update Dashboard.py
    #Bug fixed

--- Dashboard.py
+++ Dashboard.py
@@ -1,5 +1,8 @@
 import streamlit as st
 def home():
+    if not st.session_state.get("authenticated"):
+        st.error("Unauthorized access")
+        st.stop() #Bug fix
     st.sidebar.title("Navigation")
     page=st.sidebar.radio("Goto",['home','profile','settings'])
     if page=='home':
@@ -7,5 +10,6 @@ def home():
         st.write("Your personal dashboard")
     st.sidebar.divider()
     if st.sidebar.button("Logout"):
+        st.session_state.authenticated=False#bug fix
         st.session_state.page="login"
         st.rerun()
