89498e3aae02b237cb63cf2b243de3c18b4cc9d8
PATTIMANI MAHESH <mahibunny.34.s@gmail.com>
2026-03-17 22:32:31 +0530

    Update database.py
    #updated bugs

--- database.py
+++ database.py
@@ -10,7 +10,13 @@ def create_table():
                    id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,price REAL)""" )
     cursor.execute(""" CREATE TABLE IF NOT EXISTS users(
                    id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT unique,password TEXt)""")
-    cursor.execute("ALTER TABLE users ADD COLUMN failed_attempts INTEGER DEFAULT 0")
-    cursor.execute("ALTER TABLE users ADD COLUMN last_attempt REAL DEFAULT 0")
+    try:
+        cursor.execute("ALTER TABLE users ADD COLUMN failed_attempts INTEGER DEFAULT 0")
+    except:
+        pass
+    try:
+        cursor.execute("ALTER TABLE users ADD COLUMN last_attempt REAL DEFAULT 0")
+    except:
+        pass
     conn.commit()
     conn.close()
