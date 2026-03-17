51ecde724d60a322efc95674b45c28e26e1e676a
PATTIMANI MAHESH <mahibunny.34.s@gmail.com>
2026-03-17 21:28:49 +0530

    Update login.py
    #Fixing bug

--- login.py
+++ login.py
@@ -1,16 +1,18 @@
 #This repo is done for my learning of development as well as ethical hacking.
 import streamlit as st
+st.set_page_config(page_title="Auth App",layout="wide")
 from auth import login_page,signup_page
 from database import create_table
 create_table()
 from Dashboard import home
+
 if "authenticated" not in st.session_state:
     st.session_state.authenticated=False
 if "page" not in st.session_state:
     st.session_state.page="login"
-if st.session_state.page=="home" and not st.session_state.get("authenticated"):
-    st.session_state.page="login" #bug fixed
-st.set_page_config(page_title="Auth App",layout="wide")
+if not st.session_state.get("authenticated", False):
+    if st.session_state.page == "home":
+        st.session_state.page = "login"
 
 def signup():
     col1,col2,col3=st.columns([1,1,1])
@@ -20,13 +22,16 @@ def signup():
         repassword=st.text_input("create password",type="password")
         re_password=st.text_input("retype password",type="password")
         if st.button("Signup",use_container_width=True):
-            if repassword!=re_password:
+            if not name_user or not repassword:
+                st.error("Fields cannot be empty")
+            elif repassword!=re_password:
                 st.error("password not matched.")
             elif signup_page(name_user,repassword):
                 st.success("Account created successfully")
                 st.session_state.page='login'
+                st.rerun()
             else:
-                print("User already exists")
+                st.error("User already exists")
         st.divider()
         if st.button("Back_to_login",use_container_width=True):
             st.session_state.page="login"
@@ -42,6 +47,7 @@ if st.session_state.page=="login":
             if user:
                 st.success("Login successful.")
                 st.session_state.authenticated=True #Fixed buy
+                st.session_state.username = username
                 st.session_state.page="home"
                 st.rerun()
             else:
