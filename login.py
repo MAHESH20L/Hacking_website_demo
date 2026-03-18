3f055debd6ffaeba121b438214603f281caced97
PATTIMANI MAHESH <mahibunny.34.s@gmail.com>
2026-03-18 12:34:31 +0530

    Update login.py
    #updated reset password, forgot password

--- login.py
+++ login.py
@@ -6,7 +6,9 @@ from auth import login_page,signup_page
 from database import create_table
 create_table()
 from Dashboard import home
-
+import re
+def is_valid_email(email):
+    return re.match(r"[^@]+@[^@]+\.[^@]+", email)
 if "authenticated" not in st.session_state:
     st.session_state.authenticated=False
 if "page" not in st.session_state:
@@ -19,11 +21,14 @@ def signup():
     col1,col2,col3=st.columns([1,1,1])
     with col2:
         st.subheader("Signup_page")
-        name_user=st.text_input("name_user")
+        name_user=st.text_input("email")
         repassword=st.text_input("create password",type="password")
         re_password=st.text_input("retype password",type="password")
         if st.button("Signup",use_container_width=True):
-            if not name_user or not repassword:
+            if not is_valid_email(name_user):
+                st.error("Enter valid email")
+                return
+            elif not name_user or not repassword:
                 st.error("Fields cannot be empty")
             elif repassword!=re_password:
                 st.error("password not matched.")
@@ -37,68 +42,139 @@ def signup():
         if st.button("Back_to_login",use_container_width=True):
             st.session_state.page="login"
             st.rerun()
-def send_otp(email):
-    otp=str(random.randint(100000,999999)) #otp generation
-    st.session_state.otp=otp #otp sending
-    st.session_state.otp_time=time.time()
-    server=smtplib.SMTP("smtp.gmail.com",587)#smtp protocol
+def send_otp(email, otp_type="login"):
+    import random, time, smtplib
+    otp = str(random.randint(100000, 999999))
+    # store separately
+    if otp_type == "login":
+        st.session_state.login_otp = otp
+        st.session_state.login_otp_time = time.time()
+    else:
+        st.session_state.reset_otp = otp
+        st.session_state.reset_otp_time = time.time()
+    EMAIL = st.secrets["EMAIL"]
+    PASSWORD = st.secrets["EMAIL_PASS"]
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login("projectuse43@gmail.com","elnn ulzf drvq zmte")
-    server.sendmail("projectuse43@gmail.com",email,f"Your otp is {otp}")
+    server.login(EMAIL, PASSWORD)
+    server.sendmail(EMAIL, email, f"Your OTP is {otp}")
     server.quit()
 def otp_page():
-    st.subheader("Enter otp ")#OTP input
-    if "otp" not in st.session_state:
-        send_otp(st.session_state.temp_user)#sending otp
-    otp_input=st.text_input("Enter otp") #otp input
-    if st.button("verify"): #otp verification
-        #Time expiry check
-        if time.time()-st.session_state.otp_time>120:
+    st.subheader("🔐 Enter OTP")
+    if "login_otp" not in st.session_state:
+        send_otp(st.session_state.temp_user, "login")
+    otp_input = st.text_input("Enter OTP")
+    if st.button("Verify"):
+        # empty check
+        if not otp_input:
+            st.error("Enter OTP")
+            return
+        # expiry
+        if time.time() - st.session_state.login_otp_time > 120:
             st.error("OTP expired")
-            st.session_state.page="login"
+            st.session_state.login_otp = None
+            st.session_state.otp_attempts = 0
+            st.session_state.page = "login"
             st.rerun()
-        #attempt limit
+        # attempts
         if "otp_attempts" not in st.session_state:
-            st.session_state.otp_attempts=0
-        st.session_state.otp_attempts+=1
-        if st.session_state.otp_attempts>5:
+            st.session_state.otp_attempts = 0
+        st.session_state.otp_attempts += 1
+        if st.session_state.otp_attempts > 5:
             st.error("Too many attempts")
             st.stop()
-        if otp_input=st.session_state.otp:
-            st.success("Login successfull")
-            st.session_state.authenticated=True
-            st.session_state.username=st.session_state.temp_user
-            st.session_state.page="home"
-            st.session_state.otp_attempts=0
-            st.session_state.otp=None
+        # verify
+        if otp_input == st.session_state.login_otp:
+            st.success("Login successful")
+            st.session_state.authenticated = True
+            st.session_state.username = st.session_state.temp_user
+            st.session_state.page = "home"
+            # cleanup
+            st.session_state.otp_attempts = 0
+            st.session_state.login_otp = None
+            st.session_state.temp_user = None
             st.rerun()
-       else:
-            st.error("Wrong otp")
-    # 🔁 Resend OTP
+        else:
+            st.error("Wrong OTP")
+    # resend
+    if "last_otp_time" not in st.session_state:
+        st.session_state.last_otp_time = 0
     if st.button("Resend OTP"):
-        send_otp(st.session_state.temp_user)
-        st.success("OTP sent again")
-# def reset_page():
-#     st.subheader("🔑 Reset Password")
-#     email = st.text_input("Enter your email")
-#     if st.button("Send Reset OTP"):
-#         if email:
-#             st.session_state.reset_user = email
-#             send_otp(email)
-#             st.session_state.page = "reset_verify"
-#             st.rerun()
-#         else:
-#             st.error("Enter email")
-
+        if time.time() - st.session_state.last_otp_time < 30:
+            st.warning("Wait 30 seconds")
+        else:
+            send_otp(st.session_state.temp_user, "login")
+            st.session_state.last_otp_time = time.time()
+            st.success("OTP sent again")
+def reset_page():
+     st.subheader("🔑 Reset Password")
+     email = st.text_input("Enter your email")
+     if st.button("Send Reset OTP"):
+         if not email:
+            st.error("Enter email")
+            return
+         elif not is_valid_email(email):
+            st.error("Enter valid email")
+            return
+         else:
+            st.session_state.reset_user = email
+            send_otp(email, "reset")
+            st.session_state.page = "reset_verify"
+            st.rerun()
+def reset_verify_page():
+    st.subheader("🔑 Reset Password")
+    otp = st.text_input("Enter OTP")
+    new_pass = st.text_input("New Password", type="password")
+    if st.button("Update Password"):
+        # empty check
+        if not otp or not new_pass:
+            st.error("Fill all fields")
+            return
+        # expiry
+        if time.time() - st.session_state.reset_otp_time > 120:
+            st.session_state.reset_otp = None
+            st.error("OTP expired")
+            return
+        # attempts
+        if "reset_attempts" not in st.session_state:
+            st.session_state.reset_attempts = 0
+        st.session_state.reset_attempts += 1
+        if st.session_state.reset_attempts > 5:
+            st.error("Too many attempts")
+            st.stop()
+        # verify
+        if otp != st.session_state.get("reset_otp"):
+            st.error("Wrong OTP")
+            return
+        from auth import hash_pass
+        from database import connect_db
+        conn = connect_db()
+        cursor = conn.cursor()
+        hashed = hash_pass(new_pass)
+        cursor.execute(
+            "UPDATE users SET password=? WHERE username=?",
+            (hashed, st.session_state.reset_user)
+        )
+        conn.commit()
+        conn.close()
+        st.success("Password updated successfully")
+        # cleanup
+        st.session_state.reset_attempts = 0
+        st.session_state.reset_otp = None
+        st.session_state.page = "login"
+        st.rerun()
 if st.session_state.page=="login":
     col1,col2,col3=st.columns([1,1,1])
     with col2:
         st.subheader("Login_page")
-        username=st.text_input("username")
+        username=st.text_input("email")
         password=st.text_input("password",type="password")
         if st.button("Login",use_container_width=True):
             if not username or not password:
                 st.error("Username / password cannot be empty")
+            elif not is_valid_email(username):
+                st.error("Enter valid email")
+                return
             else:
                 user=login_page(username,password)
                 if user==True:
@@ -124,3 +200,7 @@ elif st.session_state.page=="home":
     home()
 elif st.session_state.page=="otp":
     otp_page()
+elif st.session_state.page == "reset":
+    reset_page()
+elif st.session_state.page == "reset_verify":
+    reset_verify_page()
